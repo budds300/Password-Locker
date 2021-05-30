a2224d05699af4ebf8ca2cc9417828520496cfc4
budds300 <budds300@gmail.com>
2021-05-30 16:24:43 +0300

    methods set

--- run.py
+++ run.py
@@ -20,4 +20,47 @@ def find_user(username):
      '''
     Funtion that finds a user by a login and returns the user
     '''
-     return User.user_exist(username)
+     return User.find_user(username) 
+def check_existing_user(username):
+      '''
+    Function that check if a user exists with that username and return a Boolean
+    '''
+      return User.user_exist(username)
+  
+def create_credentials(account_name, username, password):
+    ''' 
+    Function that creates a new credential
+    '''
+    new_credential = Credentials(account_name, username,password)
+    return new_credential
+
+def save_credential(account_name):
+    ''' 
+    saves a credential
+    '''
+    Credential.save_credential(account_name)
+    
+def delete_credential(account_name):
+    ''' 
+    deletes a credential
+    '''
+    Credentials.delete(account_name)
+    
+def find_credential(account_name):
+    ''' 
+    finds a credential by account name and returns the credential
+    '''
+    return Credentials.find_credential(account_name)
+
+def check_credential_existing(account_name):
+    '''
+    chechs if a credential exists with that account_name and retruns a boolean value
+    '''
+    return Credentials.exists(account_name)
+def display_credential():
+    ''' 
+    returns all saved credentials
+    '''
+    return Credentials.display_credential()
+
+def
